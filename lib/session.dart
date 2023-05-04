72be15366646a10292ab6fb4ba479525c43c67a6
Guidetti <gustavoo.guidetti@gmail.com>
2023-05-04 12:29:46 -0300

    finalizado

--- lib/session.dart
+++ lib/session.dart
@@ -1,23 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:projeto01/add.dart';
+import 'package:projeto01/seat.dart';
 
 class Session extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       backgroundColor: const Color.fromARGB(255, 95, 6, 0),
         appBar: AppBar(
-          title: Text('Second Page'),
+          title: Text('Sessão'),
+          backgroundColor:  Color.fromARGB(255, 241, 160, 10),
         ),
         body: Center(
-            child: Row(children: [
-          Image.asset('assets/images/session.png'),
+            child: Column(children: [
+              SizedBox(
+            height: 100,
+          ),
+           Text(
+            'Qual horário?',
+            style: TextStyle(
+              fontSize: 40,
+              foreground: Paint()
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 2
+                ..color = Color.fromARGB(255, 241, 160, 10),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
           ElevatedButton(
-            child: Text('Prosseguir'),
+            child: Text('18:30'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromARGB(255, 241, 160, 10),
+            ),
             onPressed: () {
               Navigator.push(
                   context, MaterialPageRoute(builder: (context) => Add()));
             },
           ),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            child: Text('21:00'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromARGB(255, 241, 160, 10),
+            ),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Add()));
+            },
+          ),
+          
         ])));
   }
 }
